958dc84ccd9eb5d532ee323819f4e9822081cecd
Jonathan Wright <wrightjonathan99@gmail.com>
2022-01-05 16:10:07 -0700

    Update week1.py

--- cse110/week1/week1.py
+++ cse110/week1/week1.py
@@ -2,7 +2,14 @@
 File: week1.py
 Author: Jonathan Wright
 
-Purpose: Display text to the screen.
+Purpose: input output.
 """
-
-print('hello world')
+color = input("What's your favorite color?: ")
+life = input("What is my purpose?: ")
+chicken = input("What's a ninjas favorite drink?: ")
+print('Thanks for answering my questions!')
+print("\nYour favorite color is", color, "\nWhat my purpose is:", life)
+if chicken == "water" or chicken == "wata":
+    print("Great job! You guessed right.")
+else:
+    print("A ninjas favorite drink is... Wata!\n")
