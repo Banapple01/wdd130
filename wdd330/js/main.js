5bd93b9c6d728dc7942ac1ef937b66cee2c34361
Jonathan Wright <wrightjonathan99@gmail.com>
2022-01-04 18:37:45 -0700

    Update main.js

--- wdd330/js/main.js
+++ wdd330/js/main.js
@@ -8,8 +8,10 @@ const links = [
 const ol = document.querySelector('#weekly')
 links.forEach(e => {
     console.log(e)
-    let lab = document.createElement('li').textContent = e['label'];
-    let ur = document.createElement('li').textContent = e['url'];
+    let lab = document.createElement('li')
+    lab.textContent = e['label'];
+    let ur = document.createElement('li')
+    ur.textContent = e['url'];
     ol.appendChild(lab)
     ol.appendChild(ur)
 });
