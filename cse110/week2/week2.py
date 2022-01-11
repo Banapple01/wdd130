f9d8e8d5ff49f6ad08770e7f8bf14af48fd73a1e
Jonathan Wright <wrightjonathan99@gmail.com>
2022-01-11 10:14:07 -0700

    Create week2.py

---
+++ cse110/week2/week2.py
@@ -0,0 +1,12 @@
+"""
+File: week1.py
+Author: Jonathan Wright
+
+Purpose: input output.
+"""
+first = input("What is your first name?: ")
+last = input("What is your last name?: ")
+if first == "James" and last == "Bond":
+    print("It's an honor to meet you 007.")
+else:
+    print("Your name is", last + ",", first, last)
