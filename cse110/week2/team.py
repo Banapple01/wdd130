d7f9822f4790e4f6bf86749889a1efe0f0120f7b
Jonathan Wright <wrightjonathan99@gmail.com>
2022-01-11 11:12:03 -0700

    cse110 work

---
+++ cse110/week2/team.py
@@ -0,0 +1,34 @@
+"""
+File: week1.py
+Author: Jonathan Wright
+
+Purpose: id card.
+"""
+print("Please enter the following information: \n")
+first = input("First name: ")
+last = input("Last name: ")
+email = input("Email address: ")
+phone = input("Phone number: ")
+job = input("Job title: ")
+id = input("ID Number: ")
+hair = input("Hair Color: ")
+eyes = input("Eye Color: ")
+month = input("Starting Month: ")
+train = input("Advanced Training: ")
+
+
+print(
+f"\nThe ID Card is:\n"
+f"----------------------------------------\n"
+f"{last.upper()}, {first.capitalize()}\n"
+f"{job.title()}\n"
+f"ID: {id}\n"
+
+f"{email.lower()}\n"
+f"{phone}\n"
+
+f'''
+Hair: {hair.capitalize():9}        Eyes: {eyes.capitalize()}
+Month: {month.capitalize():10}      Training: {train.capitalize()}
+----------------------------------------'''
+)
--- cse110/week2/week2.py
+++ cse110/week2/week2.py
@@ -9,4 +9,5 @@ last = input("What is your last name?: ")
 if first == "James" and last == "Bond":
     print("It's an honor to meet you 007.")
 else:
-    print("Your name is", last + ",", first, last)
+    print("Your name is", last + ",", first, last + ".")
+# print(f"Your name is {last}, {first} {last}.")
