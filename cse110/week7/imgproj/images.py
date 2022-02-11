2765126f3112c02030ee2045f97b9d0331206209
Jonathan Wright <wrightjonathan99@gmail.com>
2022-02-11 16:56:32 -0700

    finished week8 cse110

--- cse110/week7/imgproj/images.py
+++ cse110/week7/imgproj/images.py
@@ -8,9 +8,25 @@ Purpose: green screen.
 # This line imports or includes the library we will need
 from PIL import Image
 
+backgrounds = ["beach","desert","earth","field","forest","snowscape","sunset"]
+replace = ["boat","cactus","cat_small","cat","harvester","hiker","penguin","spaceshuttle"]
+
+input1 = ""
+input2 = ""
+
+while backgrounds.__contains__(input1) == False:
+    for b in backgrounds:
+        print(f'Opt: {b}')
+    input1 = input("What background image would you like? ").lower()
+    
+while replace.__contains__(input2) == False:
+    for r in replace:
+        print(f'Opt: {r}')
+    input2 = input("What replacement image would you like? ").lower()
+
 # This line opens the image and loads it into a variable called "image_original"
-image_original = Image.open("img/beach.jpg")
-image_object = Image.open("img/hiker.jpg")
+image_original = Image.open(f"img/{input1}.jpg")
+image_object = Image.open(f"img/{input2}.jpg")
 
 w, h = image_original.size
 pixels_original = image_original.load()
@@ -23,7 +39,7 @@ for j in range(w):
     for i in range(h):
         r,g,b = pixels_object[j,i]
         # print(r,g,b)
-        if(r < 90 and g > 180 and b < 90):
+        if(r < 90 and g > 160 and b < 90):
             # print(pixels_object[j,i])
             pixels_new[j,i] = pixels_original[j,i]
         else:
---
+++ cse110/week8/list.py
@@ -0,0 +1,18 @@
+"""
+File: list.py
+Author: Jonathan Wright
+
+Purpose: loops for days.
+"""
+colors = ["red", "blue", "green", "yellow"]
+
+for color in colors:
+    print(color)
+
+print("\n")
+for i in range(1,9):
+    print(i)
+
+print("\n")
+for i in range(2,21,2):
+    print(i)
